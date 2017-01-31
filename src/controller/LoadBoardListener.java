089c3876ae981868c84ac597c8405dc223913969
Paul David Bryden CESM2014 <paul.bryden.2014@uni.strath.ac.uk>
2017-01-31 22:44:20 +0000

    First Deliverable UML complete

--- src/controller/LoadBoardListener.java
+++ src/controller/LoadBoardListener.java
@@ -3,6 +3,7 @@ package controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import model.BoardFileHandler;
 import model.GameModel;
 import model.GizmoList;
 import view.Board;
@@ -11,6 +12,8 @@ public class LoadBoardListener implements ActionListener {
 GizmoList gizmoList;
 GameModel gameloop;
 Board gameBoard;
+BoardFileHandler boardHandler;
+
 @Override
 public void actionPerformed(ActionEvent e) {
 	// TODO Auto-generated method stub
--- src/controller/SaveBoardListener.java
+++ src/controller/SaveBoardListener.java
@@ -3,11 +3,12 @@ package controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import model.BoardFileHandler;
 import model.GizmoList;
 
 public class SaveBoardListener  implements ActionListener{
 GizmoList gizmoList;
-
+BoardFileHandler boardHandler;
 @Override
 public void actionPerformed(ActionEvent e) {
 	// TODO Auto-generated method stub
