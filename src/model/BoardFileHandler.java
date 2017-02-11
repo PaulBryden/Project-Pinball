b92e6f2c80daf949fd3ee68379ef2a7064c05bea
Michael Ewart <michael.ewart.2014@uni.strath.ac.uk>
2017-02-11 14:18:21 +0000

    BoardFileHandler: Fleshed out save() and createConnections() methods

--- src/model/BoardFileHandler.java
+++ src/model/BoardFileHandler.java
@@ -15,16 +15,29 @@ public class BoardFileHandler {
 		try {
 			BufferedWriter save = new BufferedWriter(new FileWriter(path));
 			ArrayList<IGizmo> list = gizmos.returnGizmoList();
+			ArrayList<String> connections = new ArrayList<>(); // connections to be made
 
 			for (IGizmo current : list) {
+//				TODO: Ensure that serializeGizmo() is providing correct output!
 				save.write(current.serializeGizmo());
-				// TODO: KeyConnections
-				// Ensure that serializeGizmo() is providing correct output!
+				
+				// Record any connections for later
+				ArrayList<IGizmo> connectedGizmos = current.getGizmosToTrigger();
+				for (IGizmo destGizmo : connectedGizmos) {
+					connections.add("Connect " + current.getID() + " " + destGizmo.getID());
+				}
+
+//				TODO: KeyConnections
+			}
+			
+			// Finally, write connections to file
+			for (String current : connections) {
+				save.write(current);
 			}
 
 			save.close();
 
-			return true; // Return true if successful
+			return true; // Save successful, return true
 
 		} catch (IOException e) {
 			System.out.println("Error writing to file " + path);
@@ -48,8 +61,7 @@ public class BoardFileHandler {
 				if (type.equals("Connect") || type.equals("KeyConnect")) {
 					connections.add(line); // Store connection info for later
 				} else {
-					IGizmo g = createGizmo(type, scan);
-					gizmos.addGizmo(g);
+					gizmos.addGizmo(createGizmo(type, scan));
 				}
 
 				scan.close();
@@ -76,23 +88,33 @@ public class BoardFileHandler {
 	}
 
 	private IGizmo createGizmo(String type, Scanner scan) {
-//		try {
-//			switch (type) {
-//			case "Square":
-//				IGizmoPhysics p = new LinearPhysics();
-//				break;
-//			case "Triangle":
-//			case "Circle":
-//			case "Ball":
-//			case "Absorber":
-//			case "LeftFlipper":
-//			case "RightFlipper":
-//			default:
-//			}
-//		} catch (Exception e) {
-//			// TODO: Catch exceptions properly
-//		}
-		return null; // TEMP
+		IGizmo gizmo = null;
+		
+		// TODO: All of these
+		try {
+			switch (type) {
+			case "Square":
+				break;
+			case "Triangle":
+				break;
+			case "Circle":
+				break;
+			case "Ball":
+				break;
+			case "Absorber":
+				break;
+			case "LeftFlipper":
+				break;
+			case "RightFlipper":
+				break;
+			default:
+			}
+		} catch (Exception e) {
+			System.out.println("Error occurred when creating Gizmo");
+			e.printStackTrace();
+		}
+
+		return gizmo;
 	}
 	
 	private void createConnections(ArrayList<String> connections, GizmoList gizmos) {
@@ -102,9 +124,19 @@ public class BoardFileHandler {
 			String type = scan.next();
 			
 			if (type.equals("Connect")) {
-				// TODO
+				String firstGizmo = scan.next();
+				String secondGizmo = scan.next();
+
+//				TODO: GizmoList function that creates connection between two gizmos in its list
+//				gizmos.createConnection(firstGizmo, secondGizmo);
+				
 			} else if (type.equals("KeyConnect")) {
-				// TODO
+				String keyString = scan.next();
+				String keyID = scan.next();
+				String keyStatus = scan.next();
+				String gizmoID = scan.next();
+				
+				// TODO: Functionality to create key connections!
 			}
 			
 			scan.close();
