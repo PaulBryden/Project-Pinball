3b9f879f5bf737f628f522c0496fda4a9bb0ff66
Raymond Dunphy <xmb14164@ds.strath.ac.uk>
2017-02-15 16:23:47 +0000

    Copy timer into runListener

--- src/controller/RunListener.java
+++ src/controller/RunListener.java
@@ -3,8 +3,21 @@ package controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.Timer;
+
+import model.Constants;
+import model.IModel;
+
 public class RunListener  implements ActionListener{
-	GameTimer timer;
+	IModel model;
+	Timer timer;
+	
+
+	public RunListener(IModel m) {
+		model = m;
+		timer = new Timer((int) (1000 * Constants.TICK_TIME), this);
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
