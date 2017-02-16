0f6cc897a11c652b8fe29d841c638f406f753126
conrad1234 <gcb14183@uni.strath.ac.uk>
2017-02-16 20:20:19 +0000

    Moved load functionality to load button and refactor of main

--- src/controller/LoadBoardListener.java
+++ src/controller/LoadBoardListener.java
@@ -1,22 +1,15 @@
 package controller;
 
-import view.Board;
 import view.MainWindow;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.List;
 
 import model.BoardFileHandler;
 import model.GameModel;
-import model.IGizmo;
 
 public class LoadBoardListener implements ActionListener{
     private MainWindow mainWindow;
-    private List<IGizmo> gizmoList;
-    private GameModel gameloop;
-    private Board gameBoard;
-    private BoardFileHandler boardHandler;
 
     public LoadBoardListener(MainWindow mainWindow){
         this.mainWindow = mainWindow;
@@ -24,6 +17,10 @@ public class LoadBoardListener implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        BoardFileHandler file = new BoardFileHandler();
+
+        file.load((GameModel) mainWindow.getBoard().getModel(),
+                "spec_save_file.txt");
         mainWindow.revalidate();
         mainWindow.repaint();
     }
--- src/main/main.java
+++ src/main/main.java
@@ -2,24 +2,17 @@ package main;
 
 import javax.swing.SwingUtilities;
 
-import model.BoardFileHandler;
 import model.GameModel;
-import model.IModel;
 import view.MainWindow;
 
 public class main {
 	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				GameModel model = new GameModel();
-				BoardFileHandler file = new BoardFileHandler();
-
-				file.load(model, "spec_save_file.txt");
-				MainWindow mainWindow = new MainWindow(model);
-				mainWindow.build();
-				model.addObserver(mainWindow.getBoard());
-			}
-		});
+		SwingUtilities.invokeLater(() -> {
+            GameModel model = new GameModel();
+            MainWindow mainWindow = new MainWindow(model);
+            mainWindow.build();
+            model.addObserver(mainWindow.getBoard());
+        });
 	}
 
 }
