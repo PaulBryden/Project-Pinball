38e9761b30522cb56d5afbda19d20f5ca8bbe9f8
Michael Ewart <michael.ewart.2014@uni.strath.ac.uk>
2017-03-14 12:11:15 +0000

    LoadBoardListener: File browsing dialogue is now shown

--- src/controller/LoadBoardListener.java
+++ src/controller/LoadBoardListener.java
@@ -4,7 +4,11 @@ import view.MainWindow;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.IOException;
+
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 
 import model.BoardFileHandler;
 import model.IModel;
@@ -21,12 +25,20 @@ public class LoadBoardListener implements ActionListener{
     	IModel model = mainWindow.getBoard().getModel();
         BoardFileHandler fh = new BoardFileHandler(model);
         model.reset();
-        try {
-			fh.load("spec_save_file.txt");
-		} catch (IOException e1) {
-			System.out.println("Error reading from file");
-			e1.printStackTrace();
-		}
-        mainWindow.getBoard().reRender();
+        JFileChooser fc = new JFileChooser();
+        int returnVal = fc.showOpenDialog(fc);
+        
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+        	String path = fc.getSelectedFile().getAbsolutePath();
+        	try {
+        		fh.load(path);
+        		mainWindow.getBoard().reRender();
+        	} catch (IOException e1) {
+        		System.out.println("Error reading from file");
+        		e1.printStackTrace();
+        		JOptionPane.showMessageDialog(mainWindow, "Error reading from file");
+        	}
+        }
+        
     }
 }
