04afcef22fc4e13fb89f255bd49f57ad232a1e87
Michael Ewart <michael.ewart.2014@uni.strath.ac.uk>
2017-03-14 12:19:00 +0000

    SaveBoardListener: actionPerformed() method filled out

--- src/controller/SaveBoardListener.java
+++ src/controller/SaveBoardListener.java
@@ -4,6 +4,13 @@ import view.MainWindow;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
+import model.BoardFileHandler;
+import model.IModel;
 
 public class SaveBoardListener implements ActionListener{
     private MainWindow mainWindow;
@@ -14,5 +21,21 @@ public class SaveBoardListener implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent e) {
+    	IModel model = mainWindow.getBoard().getModel();
+        BoardFileHandler fh = new BoardFileHandler(model);
+
+        JFileChooser fc = new JFileChooser();
+        int returnVal = fc.showSaveDialog(fc);
+        
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+        	String path = fc.getSelectedFile().getAbsolutePath();
+        	try {
+        		fh.save(path);
+        	} catch (IOException e1) {
+        		System.out.println("Error saving to file");
+        		e1.printStackTrace();
+        		JOptionPane.showMessageDialog(mainWindow, "Error saving to file");
+        	}
+        }
     }
 }
