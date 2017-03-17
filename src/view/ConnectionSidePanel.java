a64197ad4680589d44b7544998d68d8e5d1962b6
R. David Dunphy <rdd.dunphy@gmail.com>
2017-03-17 16:54:52 +0000

    Create a button factory, so buttons with icons can easily be added to toolbars and sidebars without code duplication.

--- src/view/ConnectionSidePanel.java
+++ src/view/ConnectionSidePanel.java
@@ -1,41 +1,15 @@
 package view;
 
-import java.net.URL;
-
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-
 import controller.PrimaryActionListener;
 
 public class ConnectionSidePanel extends SidePanel {
 	
 	private static final long serialVersionUID = 5258214590215188225L;
-	private PrimaryActionListener listener;
 	
 	public ConnectionSidePanel(PrimaryActionListener listener) {
 		super();
-		this.listener = listener;
-		addButton("add_connection", "Add a connection between gizmos");
-		addButton("remove_connection", "Remove a connection between gizmos");
-	}
-
-	private void addButton(String name, String toolTip) {
-		JButton button = new JButton();
-		button.setActionCommand(name);
-		button.setToolTipText(toolTip);
-		button.addActionListener(listener);
-		setButtonIcon(button, name);
-		this.add(button);
-	}
-
-	private void setButtonIcon(JButton button, String name) {
-		String iconPath = "/icons/" + name + ".png";
-		URL iconURL = RunToolBar.class.getResource(iconPath);
-		if (iconURL != null) {
-			button.setIcon(new ImageIcon(iconURL, name));
-		} else {
-			button.setText(name);
-		}
+		this.add(ButtonFactory.createButton("add_connection", "Add a connection between gizmos", listener));
+		this.add(ButtonFactory.createButton("remove_connection", "Remove a connection between gizmos", listener));
 	}
 	
 }
