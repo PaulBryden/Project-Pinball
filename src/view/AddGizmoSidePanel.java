766e2cb778c5ea7a93b4c4e337df07aae019933b
R. David Dunphy <rdd.dunphy@gmail.com>
2017-03-17 17:57:52 +0000

    Add gizmo buttons with icons to the sidepanel.

--- src/view/AddGizmoSidePanel.java
+++ src/view/AddGizmoSidePanel.java
@@ -2,55 +2,59 @@ package view;
 
 import controller.*;
 
-import javax.swing.JButton;
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JPanel;
 
 public class AddGizmoSidePanel extends SidePanel {
-	
+
 	private static final long serialVersionUID = 5655448783959185202L;
 
 	public AddGizmoSidePanel(MainWindow mainWindow) {
-        super();
-        JButton ballBtn = new JButton("Ball");
-        JButton absorberBtn = new JButton("Absorber");
-        JButton circleBtn = new JButton("Circle");
-        JButton squareBtn = new JButton("Square");
-        JButton triangleBtn = new JButton("Triangle");
-        JButton lFlipperBtn = new JButton("L.Flipper");
-        JButton rFlipperBtn = new JButton("R.Flipper");
+		super();
+		JButton ballBtn = ButtonFactory.createButton("ball", "Place a ball", new AddBallListener(mainWindow));
+		JButton absorberBtn = ButtonFactory.createButton("absorber", "Place an absorber",
+				new AddAbsorberListener(mainWindow));
+		JButton circleBtn = ButtonFactory.createButton("circle", "Place a circle gizmo",
+				new AddCircleListener(mainWindow));
+		JButton squareBtn = ButtonFactory.createButton("square", "Place a square gizmo",
+				new AddSquareListener(mainWindow));
+		JButton triangleBtn = ButtonFactory.createButton("triangle", "Place a triangle gizmo",
+				new AddTriangleListener(mainWindow));
+		JButton lFlipperBtn = ButtonFactory.createButton("left_flipper", "Place a left flipper",
+				new AddLFlipperListener(mainWindow));
+		JButton rFlipperBtn = ButtonFactory.createButton("right_flipper", "Place a right flipper",
+				new AddRFlipperListener(mainWindow));
+		this.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		c.weightx = 1;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		add(createPanel("Balls", ballBtn), c);
+		c.gridy = 1;
+		add(createPanel("Static gizmos", circleBtn, squareBtn, triangleBtn), c);
+		c.gridy = 2;
+		add(createPanel("Dynamic gizmos", lFlipperBtn, rFlipperBtn, absorberBtn), c);
+		c.gridy = 3;
+		c.fill = GridBagConstraints.BOTH;
+		c.weighty = 1;
+		add(Box.createVerticalGlue(), c);
+	}
 
-        setLayout(new GridBagLayout());
-
-        GridBagConstraints constraints = new GridBagConstraints();
-        constraints.fill = GridBagConstraints.VERTICAL;
-
-        constraints.gridy = 0;
-        ballBtn.addActionListener(new AddBallListener(mainWindow));
-        add(ballBtn, constraints);
-
-        constraints.gridy = 1;
-        absorberBtn.addActionListener(new AddAbsorberListener(mainWindow));
-        add(absorberBtn, constraints);
-
-        constraints.gridy = 2;
-        circleBtn.addActionListener(new AddCircleListener(mainWindow));
-        add(circleBtn, constraints);
-
-        constraints.gridy = 3;
-        squareBtn.addActionListener(new AddSquareListener(mainWindow));
-        add(squareBtn, constraints);
-
-        constraints.gridy = 4;
-        triangleBtn.addActionListener(new AddTriangleListener(mainWindow));
-        add(triangleBtn, constraints);
-
-        constraints.gridy = 5;
-        lFlipperBtn.addActionListener(new AddLFlipperListener(mainWindow));
-        add(lFlipperBtn, constraints);
-
-        constraints.gridy = 6;
-        rFlipperBtn.addActionListener(new AddRFlipperListener(mainWindow));
-        add(rFlipperBtn, constraints);
-    }
+	private JPanel createPanel(String title, JButton... buttons) {
+		JPanel panel = new JPanel();
+		int rows = (buttons.length + 1) / 2;
+		panel.setLayout(new GridLayout(rows, 2, 5, 5));
+		for (JButton button : buttons) {
+			panel.add(button);
+		}
+		panel.add(Box.createHorizontalGlue());
+		panel.setBorder(BorderFactory.createTitledBorder(title));
+		return panel;
+	}
 }
