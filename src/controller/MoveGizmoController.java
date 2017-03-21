0e2d434a901015672275a94a1ace46730d3eeed1
cgb14142 <cgb14142@CIS-ARROL-54.cis.strath.ac.uk>
2017-03-21 15:13:02 +0000

    fixed side panel bugs

--- src/controller/MoveGizmoController.java
+++ src/controller/MoveGizmoController.java
@@ -17,11 +17,9 @@ public class MoveGizmoController {
 
 	public void start() {
 		Board board = mainWindow.getBoard();
-		if (!(board.getState() == STATE.MOVE)) {
 			mainWindow.setSidePanel(new SidePanel(
 					"Click on a gizmo in order to move it, then click again on the grid square you want to move it to."));
 			board.setState(MOVE);
 			mainWindow.setStatusLabel("Moving Gizmo(s). Please click a gizmo on the board");
-		}
 	}
 }
